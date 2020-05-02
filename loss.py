changes in gradient computation in MSES, broadcasting along correct dimensions

loss.py
@@ -16,7 +16,7 @@ class MSE(Loss):
         return np.mean(np.power(input - target, 2))
 
     def gradient(self) -> np.ndarray:
-        return 2.0 * (self.input - self.target) / np.multiply(*self.input.shape)
+        return 2.0 * (self.input - self.target) / np.prod(self.input.shape)
 
 
 class MSES(Loss):
@@ -28,17 +28,15 @@ class MSES(Loss):
         return np.mean(np.power(self.softmax(input) - target, 2))
 
     def gradient(self) -> np.ndarray:
+        batch_size, classes = self.input.shape
         softmax = self.softmax(self.input)
-        mse_gradient = 2.0 * (softmax - self.target) / np.multiply(*self.input.shape)
-        softmax_horizontal = softmax[:, :, None]
-        softmax_vertical = softmax[:, None, :]
-        indicator = np.diag([1] * softmax.shape[1])[None, :, :]
-        gradient = (
-            mse_gradient[:, :, None]
-            * softmax_vertical
-            * (indicator - softmax_horizontal)
+        left_terms = softmax[:, None, :] - self.target[:, None, :]
+        middle_terms = softmax[:, None, :]
+        right_terms = np.diag([1] * classes)[None, :, :] - softmax[:, :, None]
+        gradients = (
+            2.0 / (batch_size * classes) * left_terms * middle_terms * right_terms
         )
-        return np.sum(gradient, axis=1)
+        return np.sum(gradients, axis=2)
 
 
 class CrossEntropy(Loss):